44ddfde89656b5466c4b5cdc040550a134ae1953
Bradlhy Machado <bmachadom@ulasalle.edu.pe>
2022-04-14 00:26:58 +0200

    Probando filosofos con hilos o programación concurrete.

--- filosofos.cpp
+++ filosofos.cpp
@@ -1,20 +1,31 @@
 #include<stdio.h>
+#include<pthread.h>
 
-void filosofo(char *arg);
+//gcc filosofos.cpp -pthread
+
+void *filosofo(void *arg);
 
 int main(void){
 	printf("FILOSOFOS\n");
+
 	char fil1[20]= "Platon";
 	char fil2[20]= "Pitagoras";
 
-	filosofo(fil1);
-	filosofo(fil2);
+	pthread_t platon, pitagoras;
+
+	pthread_create(&platon, NULL, &filosofo, &fil1);
+	pthread_create(&pitagoras, NULL, &filosofo, &fil2);	
+	
+	pthread_join(platon, NULL);
+	pthread_join(pitagoras, NULL);
 
 	return 0;
 }
 
-void filosofo(char *arg){
+void *filosofo(void *arg){
+	char *arg2=(char*)arg;
 	for(int i=0; true; i++){
-		printf("%s estoy comiendo : %d \n", arg, i);
+		printf("%s estoy comiendo : %d \n", arg2, i);
 	}
-}
+	return NULL;
+} 

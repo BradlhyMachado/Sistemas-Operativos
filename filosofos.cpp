e1020f23b0d103ce89abb1188265755811c4ff31
Bradlhy Machado <bmachadom@ulasalle.edu.pe>
2022-04-14 00:08:00 +0200

    Filsofos con programación secuencial.

--- filosofos.cpp
+++ filosofos.cpp
@@ -1,6 +1,20 @@
 #include<stdio.h>
 
+void filosofo(char *arg);
+
 int main(void){
-	printf("Filosofos");
+	printf("FILOSOFOS\n");
+	char fil1[20]= "Platon";
+	char fil2[20]= "Pitagoras";
+
+	filosofo(fil1);
+	filosofo(fil2);
+
 	return 0;
+}
+
+void filosofo(char *arg){
+	for(int i=0; i<5; i++){
+		printf("%s estoy comiendo : %d \n", arg, i);
+	}
 }

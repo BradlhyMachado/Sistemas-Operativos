bcda34e30a27253f3f7b7c639aec118e98723cb2
Bradlhy Machado <bmachadom@ulasalle.edu.pe>
2022-04-14 02:02:45 +0200

    Probando variables globales en Threads sin exclusion mutua.

--- exclusionMutua.cpp
+++ exclusionMutua.cpp
@@ -3,15 +3,18 @@
 
 //gcc filosofos.cpp -pthread
 
-#define NUM_FILOSOFOS 5
+#define NUM_FILOSOFOS 10
 
 void *filosofo(void *arg);
 
 int variable=0;
+pthread_mutex_t flag;
 
 int main(void){
 	printf("FILOSOFOS\n");
-	printf("Variable=%d/n", variable);
+	printf("Variable = %d\n", variable);
+	
+	pthread_mutex_init(&flag, NULL);
 
 	pthread_t filosofos[NUM_FILOSOFOS];
 	int ids[NUM_FILOSOFOS];
@@ -24,18 +27,21 @@ int main(void){
 		pthread_join(filosofos[i], NULL);
 	}
 
-	printf("Variable%d/n", variable);
+	pthread_mutex_destroy(&flag);
+
+	printf("Variable = %d\n", variable);
 
 	return 0;
 }
 
 void *filosofo(void *arg){
 	int arg2=*((int*)arg);
-	for(int i=0; i<5; i++){
+	for(int i=0; i<100; i++){
+		pthread_mutex_lock(&flag);
 		variable=variable+1;
-		printf("Filosofo-%d estoy comiendo : %d. variable=%d \n", arg2, i, variable);
+		//printf("Filosofo-%d estoy comiendo : %d. variable=%d \n", arg2, i, variable);
+		pthread_mutex_unlock(&flag);
 	}
 	return NULL;
 }
-
 
